ec14f8e32052bdd190339eae0cc4856e29c45b62
webdevmatias <lucasmatiassilva2022@gmail.com>
2025-10-03 23:36:02 -0300

    feat: sidebar e navegação para sessões, e adição das mesmas

---
+++ src/app/espetaculos/page.tsx
@@ -0,0 +1,3 @@
+export default function EspetaculosPage() {
+  return <h1 className="text-2xl font-bold">ESPETACULOS</h1>;
+}
---
+++ src/app/projetos/page.tsx
@@ -0,0 +1,3 @@
+export default function ProjetosPage() {
+  return <h1 className="text-2xl font-bold">PROJETOS</h1>;
+}
