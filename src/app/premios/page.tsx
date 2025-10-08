195a5e8d2cbcc01692a97249ee59f3aabf2afdc1
Vitória <jeanevitoria994@gmail.com>
2025-10-08 18:17:22 -0300

    feat: prêmios e detalhes de prêmio.

--- src/app/premios/page.tsx
+++ src/app/premios/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import BackBtn from '@/components/BackBtn';
+import Premio from '@/components/Premio';
+import React from 'react';
+import { premios } from '@/data/premios';
+
 export default function PremiosPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">PREMIOS</h1>
-    </div>
+    <section
+      className="relative bg-[#412551] min-h-screen flex flex-col items-center text-white px-4 pb-10 bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/padrao2.webp')",
+      }}
+    >
+      <BackBtn label="Prêmios" />
+
+      <div className="flex flex-col items-center justify-center gap-10 ">
+        {premios.map((premio, index) => (
+          <Premio
+            key={index}
+            title={premio.title}
+            evento={premio.evento}
+            year={premio.year}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
---
+++ src/data/premios.ts
@@ -0,0 +1,11 @@
+export const premios = [
+  {
+    title: 'Troféu Construtores da Cultura de Paz',
+    year: '2024',
+    evento: 'XIV ESPIRITOARTELIDADE',
+description: `Em 19 de julho de 2024, o Coletivo À Margem recebeu seu primeiro prêmio: o Troféu Construtores da Cultura de Paz, entregue no Teatro Santa Isabel do Recife durante o evento XIV ESPIRITOARTELIDADE: A Arte do Espírito e o Espírito da Arte.
+
+A conquista foi resultado do projeto “Oficinas Teatro Hip-Hop” (junho-julho de 2024), incentivado pela Lei Paulo Gustavo. Esse reconhecimento marcou a história do grupo, fortalecendo sua atuação na arte-educação e incentivando mais pessoas a conhecerem e se engajarem com o teatro hip-hop.`,
+    img: "/trofeu-construtores-paz.jpg"
+  }
+];
