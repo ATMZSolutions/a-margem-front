9cc787f68a12122f436a0e6e7348ef6b506208c6
webdevmatias <lucasmatiassilva2022@gmail.com>
2025-10-10 00:37:19 -0300

    chore: atualização de dependencias, ajuste da fonte dos titulos

--- src/app/espetaculos/page.tsx
+++ src/app/espetaculos/page.tsx
@@ -3,23 +3,25 @@ import Link from "next/link";
 import { slugify } from "@/utils/helpers";
 
 const espetaculosData = [
-    {
-        title: 'Muximba',
-        release: '2024',
-        img: "/muximba-coletivo.jpg"
-    },
-    {
-        title: 'Xirê',
-        release: '2023',
-        img: "/xire-coletivo.jpg"
-    }
+  {
+    title: "Muximba",
+    release: "2024",
+    img: "/muximba-coletivo.jpg",
+  },
+  {
+    title: "Xirê",
+    release: "2023",
+    img: "/xire-coletivo.jpg",
+  },
 ];
 
 export default function EspetaculosPage() {
-  const sortedShows = espetaculosData.sort((a, b) => Number(a.release) - Number(b.release)).reverse()
+  const sortedShows = espetaculosData
+    .sort((a, b) => Number(a.release) - Number(b.release))
+    .reverse();
 
   const espetaculos = sortedShows.map((item, index) => {
-    const slugname = slugify(item.title)
+    const slugname = slugify(item.title);
     const href = `/espetaculos/${slugname}`;
 
     return (
@@ -29,14 +31,18 @@ export default function EspetaculosPage() {
         style={{ color: "white" }}
         className="flex flex-col cursor-pointer mx-2"
       >
-          <img src={item.img} alt={`Evento de ${item.title}`} className="h-[200px] md:h-[300px] max-w-[300px] md:max-w-[500px] object-cover" />
-          <div className="flex flex-row justify-between">
-            <span className="text-black">{item.release}</span>
-            <span className="text-4xl md:text-5xl text-black">{item.title}</span>
-          </div>
+        <img
+          src={item.img}
+          alt={`Evento de ${item.title}`}
+          className="h-[200px] md:h-[300px] max-w-[300px] md:max-w-[500px] object-cover"
+        />
+        <div className="flex flex-row justify-between">
+          <span className="text-black">{item.release}</span>
+          <span className="text-4xl md:text-4xl text-black font-sedgwick uppercase tracking-widest mt-2">{item.title}</span>
+        </div>
       </Link>
-    )
-  })
+    );
+  });
 
   return (
     <section
@@ -45,11 +51,10 @@ export default function EspetaculosPage() {
         backgroundImage: "url('/padrao2.webp')",
       }}
     >
-      <BackBtn label="Espetáculos" />
-      <div className='max-w-xl md:max-w-2xl mt-40 flex flex-col gap-12'>
-          {espetaculos}
+      <BackBtn label="Voltar" color="text-black" />
+      <div className="max-w-xl md:max-w-2xl mt-40 flex flex-col gap-12">
+        {espetaculos}
       </div>
     </section>
   );
 }
-
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+// tailwind.config.ts
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sedgwick: ["var(--font-sedgwick)", "cursive"], // agora Tailwind reconhece
+      },
+    },
+  },
+};
